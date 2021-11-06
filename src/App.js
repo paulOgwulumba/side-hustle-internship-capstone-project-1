e1252a98add8884cbecc022e5872a1ee27159347
Benjamin Efenudu <benjaminefenudu@gmail.com>
2021-11-06 05:34:26 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer'
 import './App.css';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
